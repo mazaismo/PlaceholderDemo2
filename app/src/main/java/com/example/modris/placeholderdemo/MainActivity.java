2a9cdcac31592b6d6fbcdaa20b37ed9260f9b4b7
Modris Ābrams <brainstormer57@googlemail.com>
2019-07-19 10:46:36 +0300

    placeholder demo

--- app/src/main/java/com/example/modris/placeholderdemo/MainActivity.java
+++ app/src/main/java/com/example/modris/placeholderdemo/MainActivity.java
@@ -1,52 +1,76 @@
 package com.example.modris.placeholderdemo;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private TextInputLayout textInputEmail;
+    private TextInputLayout textInputUsername;
+    private TextInputLayout textInputPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        textInputEmail = findViewById(R.id.text_input_email);
+        textInputUsername = findViewById(R.id.text_input_username);
+        textInputPassword = findViewById(R.id.text_input_password);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    private boolean validateEmail() {
+        String emailInput = textInputEmail.getEditText().getText().toString().trim();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (emailInput.isEmpty()) {
+            textInputEmail.setError("Field can't be empty");
+            return false;
+        } else {
+            textInputEmail.setError(null);
             return true;
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    private boolean validateUsername() {
+        String usernameInput = textInputUsername.getEditText().getText().toString().trim();
+
+        if (usernameInput.isEmpty()) {
+            textInputUsername.setError("Field can't be empty");
+            return false;
+        } else if (usernameInput.length() > 15) {
+            textInputUsername.setError("Username too long");
+            return false;
+        } else {
+            textInputUsername.setError(null);
+            return true;
+        }
+    }
+
+    private boolean validatePassword() {
+        String passwordInput = textInputPassword.getEditText().getText().toString().trim();
+
+        if (passwordInput.isEmpty()) {
+            textInputPassword.setError("Field can't be empty");
+            return false;
+        } else {
+            textInputPassword.setError(null);
+            return true;
+        }
+    }
+
+    public void confirmInput(View v) {
+        if (!validateEmail() | !validateUsername() | !validatePassword()) {
+            return;
+        }
+
+        String input = "Email: " + textInputEmail.getEditText().getText().toString();
+        input += "\n";
+        input += "Username: " + textInputUsername.getEditText().getText().toString();
+        input += "\n";
+        input += "Password: " + textInputPassword.getEditText().getText().toString();
+
+        Toast.makeText(this, input, Toast.LENGTH_SHORT).show();
     }
 }
